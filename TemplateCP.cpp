c1dfbed45a4d78c64870d86892a1bc78b1394986
Matheus Rodrigues <matheusnoom@gmail.com>
2022-07-06 01:17:40 -0300

    templateCP

---
+++ TemplateCP.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ar array
+
+void solve(){
+
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	//Numero de casos de teste
+	int t;
+	cin >> t;
+	
+	//loop dos casos de teste
+	while(t--) {
+		solve();
+	}
+}
